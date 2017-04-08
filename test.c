67ddc783e67830ff92ccb80ae23ecda1d4ab1d16
gotham19 <gowthamanv@globussoft.in>
2017-04-08 18:39:45 +0530

    changed code

--- test.c
+++ test.c
@@ -2,13 +2,9 @@
 #include<conio.h>
 void main()
 {
-<<<<<<< HEAD
+
 printf("this is tester");
-printf(" gowtham test");
-=======
-printf("this is test");
 printf("this is bala");
->>>>>>> aa30db2d3b6660b94656736595770afe4b66a871
 getch();
 exit(0);
 }
