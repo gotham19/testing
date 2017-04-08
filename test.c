c794cdfbf11600680eddb9ced89d31f58c684b32
gotham19 <gowthamanv@globussoft.in>
2017-04-08 18:37:14 +0530

    fixed code

--- test.c
+++ test.c
@@ -2,17 +2,10 @@
 #include<conio.h>
 void main()
 {
-<<<<<<< HEAD
 printf("this is tester");
 printf(" gowtham test");
-=======
 printf("this is test");
 printf("this is bala");
->>>>>>> aa30db2d3b6660b94656736595770afe4b66a871
 getch();
-<<<<<<< HEAD
 clrscr();
-=======
-exit(0);
->>>>>>> 888eb700669d5e873187d1f214f180a3b8c41e68
 }
